e0f1a12e62cd967bd22566e64a9ab16347546a57
Mark Brettin <mark@jackrabbitsystems.com>
2018-12-04 07:52:05 -0700

    Implements code syntax highlighting using rouge.

--- app/controllers/highlighter_controller.rb
+++ app/controllers/highlighter_controller.rb
@@ -1,5 +1,5 @@
 class HighlighterController < ApplicationController
   def index
-    
+
   end
 end
--- app/helpers/highlighter_helper.rb
+++ app/helpers/highlighter_helper.rb
@@ -1,2 +1,14 @@
 module HighlighterHelper
+  def options_for_language(selected_lang)
+    selected = selected_lang.blank? ? '' : selected_lang
+    #options = ['', 'ruby', 'java', 'php', 'perl', 'python']
+    options = Rouge::Lexer.all.map(&:tag).sort
+    options_for_select(options, selected)
+  end
+
+  def rouge(text, language)
+    formatter = Rouge::Formatters::HTML.new(css_class: 'highlight-github')
+    lexer = Rouge::Lexer.find(language)
+    formatter.format(lexer.lex(text))
+  end
 end
