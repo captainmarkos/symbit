006188aedc91965683320e27c3dde855207b1604
Mark Brettin <mark@jackrabbitsystems.com>
2018-12-05 09:02:52 -0700

    Makes an ajax request to get the highlighted code.

--- app/controllers/highlighter_controller.rb
+++ app/controllers/highlighter_controller.rb
@@ -1,5 +1,26 @@
 class HighlighterController < ApplicationController
+  protect_from_forgery with: :null_session
+
   def index
 
+  end
+
+  def rouge
+    text = rouge_params[:text]
+    language = rouge_params[:language]
+    result = { highlighted_text: rougify(text, language) }
+    render json: result
+  end
+
+  private
+
+  def rouge_params
+    params.permit(:language, :text)
+  end
+
+  def rougify(text, language)
+    formatter = Rouge::Formatters::HTML.new(css_class: 'highlight-github')
+    lexer = Rouge::Lexer.find(language)
+    formatter.format(lexer.lex(text))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :highlighter, only: [:index]
+  resources :highlighter, only: [:index] do
+    collection do
+      post :rouge
+    end
+  end
 end
