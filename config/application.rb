ef41d38d4b6bbe8b73d791bac32e48620712584e
Mark Brettin <mark@jackrabbitsystems.com>
2019-04-19 17:41:44 -0600

    application.rb mods

--- config/application.rb
+++ config/application.rb
@@ -17,9 +17,5 @@ module Symbit
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # heroku
-    config.assets.initialize_on_precompile = false
-    config.serve_static_assets = true
   end
 end
